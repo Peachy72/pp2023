1fc779c41b013d36de66cdd78c7f61cdcb35100a
Dao Tuyet Ngan <ngandt.bi12-311@st.usth.edu.vn>
2023-03-29 00:22:02 +0700

    Compressed data into zip file and decompressing

--- pw5/mainn.py
+++ pw5/mainn.py
@@ -1,50 +1,64 @@
 from innput import Input
 from ooutput import Output
 import math
-
+import os
+import zipfile
+from domain.Student import Student
+from domain.Course import Course
+from domain.Mark import Mark
 students = []
 courses = []
 marks = []
 gpas = []
 
 def main():
-    # upon program start, check if students.dat exists
+    # unzip file and split into 3 files
     try:
-        r = open("students.dat", "r")
-        # if file exists, read file and add students, courses, and marks to their respective lists
-        for line in r:
-            if line == "Students \n":
-                for line in r:
-                    if line == "Courses \n":
-                        break
-                    else:
-                        student = Student(line.split(" ")[0], line.split(" ")[1], line.split(" ")[2])
-                        students.append(student)
-            elif line == "Courses \n":
-                for line in r:
-                    if line == "Marks \n":
-                        break
-                    else:
-                        course = Course(line.split(" ")[0], line.split(" ")[1])
-                        courses.append(course)
-            elif line == "Marks \n":
-                for line in r:
-                    mark = Mark(line.split(" ")[0], line.split(" ")[1], line.split(" ")[2])
-                    marks.append(mark)
+        with zipfile.ZipFile("data.zip", 'r') as zip_ref:
+            # extract into current directory
+            zip_ref.extractall()
+    except:
+        print("File not found")
+    # check if files exist
+    try:
+        r = open("students.txt", "r")
         r.close()
-    except FileNotFoundError:
-    # create students.txt 
-    w = open("students.txt", "a")
-    w.write("ID\tName\tDOB\n")
-    w.close()
-    # create courses.txt
-    w = open("courses.txt", "a")
-    w.write("ID\tName\n")
-    w.close()
-    # create marks.txt
-    w = open("marks.txt", "a")
-    w.write("Student ID\tCourse ID\tMark\n")
-    w.close()
+        r = open("courses.txt", "r")
+        r.close()
+        r = open("marks.txt", "r")
+        r.close()
+    except:
+       # open files if they don't exist
+        w = open("students.txt", "w")
+        w.close()
+        w = open("courses.txt", "w")
+        w.close()
+        w = open("marks.txt", "w")
+        w.close()
+    # load data from files
+    try: 
+        r = open("students.txt", "r")
+        for line in r.readlines():
+            student = line.split()
+            # initialize student object
+            student = Student(student_id=student[0], student_name=student[1], student_dob=student[2])
+            students.append(student)
+        r.close()
+        r = open("courses.txt", "r")
+        for line in r.readlines():
+            course = line.split()
+            course = Course(course_name=course[1], course_id=course[0])
+            courses.append(course)
+        r.close()
+        r = open("marks.txt", "r")
+        for line in r.readlines():
+            mark = line.split()
+            mark = Mark(student_id=mark[0], course_id=mark[1], mark=mark[2])
+            marks.append(mark)
+        r.close()
+        print("Data loaded successfully")
+    except:
+        print("Error loading data")
     while True:
         print(chr(27) + "[2J\n")
         print("Student mark management system\n 1. Add a student\n 2. Add a course\n 3. Add a mark\n 4. List students\n 5. List courses\n 6. List marks\n 7. Get average mark\n 8. Sort students by GPA\n 9. Exit")
@@ -52,8 +66,6 @@ def main():
         if choice == "1":
             Input.add_student(students)
         elif choice == "2":
-            w = open("courses.txt", "a")
-            w.write("ID\tName\n")
             Input.add_course(courses)
         elif choice == "3":
             Input.add_mark(marks, students, courses)
@@ -72,18 +84,18 @@ def main():
             Output.sort_students_by_gpa(students, gpas, marks)
         elif choice == "9":
             print("Exiting...")
-            # compress file before exiting program. Compress all files into one file "students.dat"
-            w = open("students.dat", "a")
-            w.write("Students\n")
-            for student in students:
-                w.write(student.id + " " + student.name + " " + student.dob + "\n")
-            w.write("Courses\n")
-            for course in courses:
-                w.write(course.id + " " + course.name + "\n")
-            w.write("Marks\n")
-            for mark in marks:
-                w.write(mark.student_id + " " + mark.course_id + " " + str(mark.mark) + "\n")
-            w.close()
+            # compress all files into a zip file
+            zip_file = zipfile.ZipFile("data.zip", "w")
+            zip_file.write("students.txt")
+            zip_file.write("courses.txt")
+            zip_file.write("marks.txt")
+            zip_file.close()
+            print("Data compressed")
+            # delete all other files
+            os.remove("students.txt")
+            os.remove("courses.txt")
+            os.remove("marks.txt")
+            print("Temporary files deleted")
             break
         else:
             print("Invalid choice")
